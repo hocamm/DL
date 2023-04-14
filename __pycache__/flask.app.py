26f9723b9fa259966f92649e49ccfa39871e1ce6
PFSV <96779838+PFSV@users.noreply.github.com>
2023-04-14 15:02:50 +0900

    Update flask.app.py

--- __pycache__/flask.app.py
+++ __pycache__/flask.app.py
@@ -1,18 +1,23 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
 from flask import Flask
+from flask import request
+from flask import jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def string_return():
-    return '문자열 반환해야 합니당'
+    return "ML 서버 열심히 만들자"
 
-# 문자열 반환 테스트용
+
 @app.route('/sick')
 def breadpage():
    return '식빵맨'
 
-@app.route("/curry") 		
-def curry():			
+@app.route("/curry")
+def curry():
     return '''<!DOCTYPE HTML><html>
   <head>
     <title>카레빵맨 이미지 받아랑!!!</title>
@@ -22,3 +27,15 @@ def curry():
     <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrfTgmkoNaKpFBUZ_DmdzbkGc66dsch5CWYg&usqp=CAU"
   </body>
 </html>'''
+
+
+
+
+@app.route('/bread', methods=['GET'])
+def test_get():
+    bread_receive = request.args.get('breadname')
+    return f'이것의 이름은 {bread_receive}'
+
+
+
+
