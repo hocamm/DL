1d11f42bd7f1955965c36a68c77fc0e34abd65ad
PFSV <96779838+PFSV@users.noreply.github.com>
2023-04-14 14:40:48 +0900

    Update flask.app.py

--- __pycache__/flask.app.py
+++ __pycache__/flask.app.py
@@ -10,3 +10,15 @@ def string_return():
 @app.route('/sick')
 def breadpage():
    return '식빵맨'
+
+@app.route("/curry") 		
+def curry():			
+    return '''<!DOCTYPE HTML><html>
+  <head>
+    <title>카레빵맨 이미지 받아랑!!!</title>
+  </head>
+  <body>
+    <h1>Curry bread man</h1>
+    <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrfTgmkoNaKpFBUZ_DmdzbkGc66dsch5CWYg&usqp=CAU"
+  </body>
+</html>'''
